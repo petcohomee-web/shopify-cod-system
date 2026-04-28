bffa7f3073f60d7d1883582808424b504fac27c1
Movika <petcohomee@gmail.com>
2026-04-28 23:02:19 +0200

    add meta capi purchase

--- backend/src/app/api/create-order/route.ts
+++ backend/src/app/api/create-order/route.ts
@@ -60,6 +60,8 @@ Telefon: ${phone}
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
+  test_event_code: "TEST73658",
+  data: [
           client_id: process.env.SHOPIFY_CLIENT_ID,
           client_secret: process.env.SHOPIFY_CLIENT_SECRET,
           grant_type: "client_credentials",
