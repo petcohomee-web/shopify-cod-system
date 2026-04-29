c9c9465aaece157076bf664243ac5387ba2b37e3
Movika <petcohomee@gmail.com>
2026-04-29 03:09:24 +0200

    fix POST form-config

--- backend/src/app/api/form-config/route.ts
+++ backend/src/app/api/form-config/route.ts
@@ -27,3 +27,23 @@ export async function GET() {
     );
   }
 }
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const { language, fields, design } = body;
+
+    await sql`
+      INSERT INTO form_config (language, fields, design)
+      VALUES (${language}, ${fields}, ${design});
+    `;
+
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { success: false, error: "DB error" },
+      { status: 500 }
+    );
+  }
+}
